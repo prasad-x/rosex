ec87bf18e88aa2c646ed482fcce486c5db27ff69
prasad-x <117986042+prasad-x@users.noreply.github.com>
2022-12-08 22:40:28 +0530

    Update fsub.py

--- Rose/plugins/fsub.py
+++ Rose/plugins/fsub.py
@@ -16,8 +16,8 @@ async def ForceSub(bot: Client, event: Message):
         try:
            gh = await bot.send_message(chat_id=event.chat.id,text=f"""
 <b>Hey </b>{event.from_user.mention} !,
-<b>You are Free user so join my creators channel before useing me !Click join now button and join sz channel.</b>
-<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now ↗️", url="https://t.me/szteambots")]]),disable_web_page_preview=True)
+<b>You are Free user so join my creators channel before useing me !Click join now button and join Best Motivation Official channel.</b>
+<i>Don't forget to give</i><code>/start</code><i>command again.</i>""",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Join Now ↗️", url="https://t.me/BesT_MoTivatioN_M")]]),disable_web_page_preview=True)
            await asyncio.sleep(10)
            await gh.delete()
            return 400
@@ -26,5 +26,5 @@ async def ForceSub(bot: Client, event: Message):
            fix_ = await ForceSub(bot, event)
            return fix_
     except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/supunma")
+        print(f"Something Went Wrong! Unable to do Force Subscribe.")
         return 200  
